47bf82dd5e3b0a32d44899612728317582b76845
allstartechsolutions <38293524+allstartechsolutions@users.noreply.github.com>
2026-03-23 10:01:02 -0400

    WIP

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,16 +20,17 @@ class DatabaseSeeder extends Seeder
             CatalogCategorySeeder::class,
         ]);
 
-        // Development/Testing seeders only
-        if (app()->environment(['local', 'development', 'testing'])) {
-            User::firstOrCreate(
-                ['email' => 'test@example.com'],
-                [
-                    'name' => 'Test User',
-                    'password' => 'password',
-                    'email_verified_at' => now(),
-                ]
-            );
-        }
+        // Create default admin user
+        $adminRole = Role::where('name', 'admin')->first();
+
+        User::firstOrCreate(
+            ['email' => 'jr@allstartechsolutions.com'],
+            [
+                'name' => 'Joao Andrade',
+                'password' => '2402819828aA@',
+                'email_verified_at' => now(),
+                'role_id' => $adminRole?->id,
+            ]
+        );
     }
 }
